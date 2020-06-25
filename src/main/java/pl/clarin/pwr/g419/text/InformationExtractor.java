3ab22334eafd63cb9d01bee44071c5f6116aa905
Michał Olek <michal.olek@gmail.com>
2020-06-25 09:52:52 +0200

    Przeniesienie do ExtractorPeople funkcji dot. rozpoznawania osób

--- src/main/java/pl/clarin/pwr/g419/text/InformationExtractor.java
+++ src/main/java/pl/clarin/pwr/g419/text/InformationExtractor.java
@@ -1,21 +1,19 @@
 package pl.clarin.pwr.g419.text;
 
 import com.google.common.collect.Lists;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import pl.clarin.pwr.g419.HasLogger;
 import pl.clarin.pwr.g419.kbase.lexicon.CompanyLexicon;
-import pl.clarin.pwr.g419.kbase.lexicon.PersonNameLexicon;
 import pl.clarin.pwr.g419.struct.*;
 import pl.clarin.pwr.g419.text.annotator.*;
 import pl.clarin.pwr.g419.text.extractor.ExtractorPeople;
 import pl.clarin.pwr.g419.text.lemmatizer.CompanyLemmatizer;
 import pl.clarin.pwr.g419.text.normalization.NormalizerCompany;
+
+import static pl.clarin.pwr.g419.utils.DateUtils.parseDate;
 
 @Slf4j
 public class InformationExtractor implements HasLogger {
@@ -35,7 +33,7 @@ public class InformationExtractor implements HasLogger {
   CompanyLexicon companyLexicon = new CompanyLexicon();
   NormalizerCompany companyNormalizer = new NormalizerCompany();
   CompanyLemmatizer companyLemmatizer = new CompanyLemmatizer();
-  PersonNameLexicon personNameLexicon = new PersonNameLexicon();
+
 
   ExtractorPeople extractorPeople = new ExtractorPeople();
 
@@ -59,7 +57,6 @@ public class InformationExtractor implements HasLogger {
     getCompany(document).ifPresent(metadata::setCompany);
 
     extractorPeople.extract(document).ifPresent(metadata::setPeople);
-    metadata.setPeople(getPoeple(document));
 
     //assignDefaultSignDate(metadata);
 
@@ -77,55 +74,6 @@ public class InformationExtractor implements HasLogger {
     }
   }
 
-  private List<FieldContext<Person>> getPoeple(final HocrDocument document) {
-    // jeśli mamy stronę z podpisami to sprawdźmy czy tylko z niej można coś sensowego wyciągnąć ...
-    if (document.getPageNrWithSigns() != 0) {
-      final var resultForSignsPage = getPeopleForAnnotations(document.getAnnotationsForSignsPage());
-      if (resultForSignsPage.size() > 0) {
-        return resultForSignsPage;
-      }
-    }
-    // .. jeśli nie można to prcoesujemy standardowo
-    return getPeopleForAnnotations(document.getAnnotations());
-  }
-
-  private List<FieldContext<Person>> getPeopleForAnnotations(final AnnotationList annotations) {
-    return annotations
-        .filterByType(AnnotatorPersonHorizontal.PERSON)
-        .removeNested()
-        .sortByLoc()
-        .stream()
-        .map(an -> new FieldContext<>(strToPerson(an.getNorm()), an.getContext(), an.getSource()))
-        .collect(Collectors.toMap(o -> personToFirstLastName(o.getField()), Function.identity(),
-            (p1, p2) -> p1.getField().getName().length() > p2.getField().getName().length() ? p1 : p2))
-        // take the one with longer name or latter occurance
-        .values().stream().collect(Collectors.toList());
-  }
-
-  private String personToFirstLastName(final Person p) {
-    final String[] parts = p.getName().toLowerCase().split(" ");
-    if (parts.length == 1) {
-      return parts[0];
-    }
-    return parts[0] + " " + parts[parts.length - 1];
-  }
-
-  private Person strToPerson(final String str) {
-    final Person person = new Person();
-    final String[] parts = str.split("[|]");
-    if (parts.length > 0) {
-      person.setDate(parseDate(parts[0]));
-    }
-    if (parts.length > 1) {
-      person.setRole(parts[1].toLowerCase());
-    }
-    if (parts.length > 2) {
-      final String name = parts[2]
-          .replaceAll("[ ]*-[ ]*", "-");
-      person.setName(personNameLexicon.approximate(name));
-    }
-    return person;
-  }
 
   private Optional<Pair<FieldContext<Date>, FieldContext<Date>>> getPeriod(
       final HocrDocument document) {
@@ -186,14 +134,11 @@ public class InformationExtractor implements HasLogger {
 
   private FieldContext<String> getSignsPage(final HocrDocument document) {
     final List<Pair<Integer, Integer>> linesWithPodpisy = findLinesWithSigns(document);
-    int pageNrWithSigns = 0;
     final Pair<Integer, Integer> lineWithSigns;
 
     if ((linesWithPodpisy == null) || (linesWithPodpisy.size() == 0)) {
       return new FieldContext<String>("0", "", null);
-
     } else if (linesWithPodpisy.size() == 1) {
-
       lineWithSigns = linesWithPodpisy.get(0);
     } else {
       lineWithSigns = linesWithPodpisy.stream()
@@ -201,17 +146,12 @@ public class InformationExtractor implements HasLogger {
     }
 
     final String line = document.getLineInPage(lineWithSigns.getRight(), lineWithSigns.getLeft() - 1);
-
-
+    int pageNrWithSigns = 0;
     if (isThisLineWithSignsActually(line)) {
       pageNrWithSigns = lineWithSigns.getLeft();
     }
-
     document.setPageNrWithSigns(pageNrWithSigns);
-
-
     return new FieldContext<String>("" + pageNrWithSigns, "", null);
-
   }
 
   private boolean isThisLineWithSignsActually(final String line) {
@@ -228,7 +168,6 @@ public class InformationExtractor implements HasLogger {
     final Set<String> skipWords = Set.of("s", "a", "grupy", "kapitałowej", "data", "wchodzących", "skład");
 
     final Set<String> rest = new HashSet<>();
-
     int hitsCounter = 0;
     for (final String w : words) {
       if (highlightWords.contains(w)) {
@@ -254,14 +193,6 @@ public class InformationExtractor implements HasLogger {
     return true;
   }
 
-
-  private Date parseDate(final String str) {
-    try {
-      return new SimpleDateFormat("yyyy-MM-dd").parse(str);
-    } catch (final Exception e) {
-      return null;
-    }
-  }
 
   public List<Pair<Integer, Integer>> findLinesWithSigns(final HocrDocument document) {
     final List<Pair<Integer, Integer>> result = new ArrayList<>();
--- src/main/java/pl/clarin/pwr/g419/text/extractor/ExtractorPeople.java
+++ src/main/java/pl/clarin/pwr/g419/text/extractor/ExtractorPeople.java
@@ -2,15 +2,81 @@ package pl.clarin.pwr.g419.text.extractor;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import pl.clarin.pwr.g419.kbase.lexicon.PersonNameLexicon;
+import pl.clarin.pwr.g419.struct.AnnotationList;
 import pl.clarin.pwr.g419.struct.FieldContext;
 import pl.clarin.pwr.g419.struct.HocrDocument;
 import pl.clarin.pwr.g419.struct.Person;
+import pl.clarin.pwr.g419.text.annotator.AnnotatorPersonHorizontal;
+
+import static pl.clarin.pwr.g419.utils.DateUtils.parseDate;
 
 public class ExtractorPeople implements IExtractor<List<FieldContext<Person>>> {
 
+  PersonNameLexicon personNameLexicon = new PersonNameLexicon();
+
+
   @Override
   public Optional<List<FieldContext<Person>>> extract(final HocrDocument document) {
-    return Optional.empty();
+    return getPeople(document);
   }
+
+  private Optional<List<FieldContext<Person>>> getPeople(final HocrDocument document) {
+    // jeśli mamy stronę z podpisami to sprawdźmy czy tylko z niej można coś sensowego wyciągnąć ...
+    if (document.getPageNrWithSigns() != 0) {
+      final var resultForSignsPage = getPeopleForAnnotations(document.getAnnotationsForSignsPage());
+      if (resultForSignsPage.size() > 0) {
+        return Optional.of(resultForSignsPage);
+      }
+    }
+    // .. jeśli nie można to procesujemy standardowo
+    final List<FieldContext<Person>> result = getPeopleForAnnotations(document.getAnnotations());
+    if (result.size() == 0) {
+      return Optional.empty();
+    }
+
+    return Optional.of(result);
+  }
+
+  private List<FieldContext<Person>> getPeopleForAnnotations(final AnnotationList annotations) {
+    return annotations
+        .filterByType(AnnotatorPersonHorizontal.PERSON)
+        .removeNested()
+        .sortByLoc()
+        .stream()
+        .map(an -> new FieldContext<>(strToPerson(an.getNorm()), an.getContext(), an.getSource()))
+        .collect(Collectors.toMap(o -> personToFirstLastName(o.getField()), Function.identity(),
+            (p1, p2) -> p1.getField().getName().length() > p2.getField().getName().length() ? p1 : p2))
+        // take the one with longer name or latter occurance
+        .values().stream().collect(Collectors.toList());
+  }
+
+  private String personToFirstLastName(final Person p) {
+    final String[] parts = p.getName().toLowerCase().split(" ");
+    if (parts.length == 1) {
+      return parts[0];
+    }
+    return parts[0] + " " + parts[parts.length - 1];
+  }
+
+  private Person strToPerson(final String str) {
+    final Person person = new Person();
+    final String[] parts = str.split("[|]");
+    if (parts.length > 0) {
+      person.setDate(parseDate(parts[0]));
+    }
+    if (parts.length > 1) {
+      person.setRole(parts[1].toLowerCase());
+    }
+    if (parts.length > 2) {
+      final String name = parts[2]
+          .replaceAll("[ ]*-[ ]*", "-");
+      person.setName(personNameLexicon.approximate(name));
+    }
+    return person;
+  }
+
 
 }
