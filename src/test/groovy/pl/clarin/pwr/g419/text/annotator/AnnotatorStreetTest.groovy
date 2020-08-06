cf8d2402e8310c2c9d3d4af347fb9958f4bd35a9
Michał <michal.olek@pwr.edu.pl>
2020-08-06 18:24:05 +0200

    Dodanie testów

--- src/test/groovy/pl/clarin/pwr/g419/text/annotator/AnnotatorStreetTest.groovy
+++ src/test/groovy/pl/clarin/pwr/g419/text/annotator/AnnotatorStreetTest.groovy
@@ -26,43 +26,46 @@ class AnnotatorStreetTest extends Specification {
                     .collect { it.getNorm() } as Set == norm as Set
 
         where:
-            text                                               || norm
-            "ul. Słowiańska "                                  || ["Słowiańska:"]
-            " znajduje się na ul. Słowiańskiej we Wrocławiu"   || ["Słowiańska:"]
-            " mieści się przy ul. Słowiańskiej we Wrocławiu"   || ["Słowiańska:"]
-            " znajduje się ul. Opolska we Wrocławiu"           || ["Opolska:"]
-            " znajduje się przy ul. Opolskiej na Nowym Dworze" || ["Opolska:"]
-            " znajduje się przy ul. Hutniczej we Warszawie"    || ["Hutnicza:"]
-            " zlokalizowane jest przy ul. Znojnej"             || ["Znojna:"]
-            "al. Brygadzistów "                                || ["Brygadzistów:"]
-            " jest na al. Nyskiej "                            || ["Nyska:"]
+            text                                                   || norm
+            "ul. Słowiańska "                                      || ["SŁOWIAŃSKA:"]
+            " znajduje się na ul. Słowiańskiej we Wrocławiu"       || ["SŁOWIAŃSKA:"]
+            " mieści się przy ul. Słowiańskiej we Wrocławiu"       || ["SŁOWIAŃSKA:"]
+            " znajduje się ul. Opolska we Wrocławiu"               || ["OPOLSKA:"]
+            " znajduje się przy ul. Opolskiej na Nowym Dworze"     || ["OPOLSKA:"]
+            " znajduje się przy ul. Hutniczej we Warszawie"        || ["HUTNICZA:"]
+            " zlokalizowane jest przy ul. Znojnej"                 || ["ZNOJNA:"]
+            "al. Brygadzistów "                                    || ["BRYGADZISTÓW:"]
+            " jest na al. Nyskiej "                                || ["NYSKA:"]
 
-            "ul. Słowiańska 15 we Wrocławiu"                   || ["Słowiańska:15"]
-            "przy ul. Opolskiej 21 we Wrocławiu"               || ["Opolska:21"]
-            " na ul. Hutniczej 134 w Poznaniu "                || ["Hutnicza:134"]
+            "ul. Słowiańska 15 we Wrocławiu"                       || ["SŁOWIAŃSKA:15"]
+            "przy ul. Opolskiej 21 we Wrocławiu"                   || ["OPOLSKA:21"]
+            " na ul. Hutniczej 134 w Poznaniu "                    || ["HUTNICZA:134"]
 
-            "ul. Obrońców Westerplatte "                       || ["Obrońców Westerplatte:"]
+            "ul. Obrońców Westerplatte "                           || ["OBROŃCÓW WESTERPLATTE:"]
 
-            "ul. Słowiańska 13b"                               || ["Słowiańska:13b"]
-            "ul. Słowiańska 19A"                               || ["Słowiańska:19A"]
+            "ul. Słowiańska 13b"                                   || ["SŁOWIAŃSKA:13b"]
+            "ul. Słowiańska 19A"                                   || ["SŁOWIAŃSKA:19A"]
 
-            "ul. Słowiańska 13/19"                             || ["Słowiańska:13/19"]
-            "ul. Słowiańska 13D/19"                            || ["Słowiańska:13D/19"]
-            "na ul. Opolskiej 2-4"                             || ["Opolska:2-4"]
-            ///"przy ul. Łąkowej 39-44"                           || ["Łąkowa:39-44"]  -- Orzeszkowej
-            "przy ul. Kasprzaka 39-44"                         || ["Kasprzaka:39-44"]
-            "przy ul. Kasprzaka 302-304 ."                     || ["Kasprzaka:302-304"]
-            "przy ul. Mihai Bravu 302-304 ."                   || ["Mihai Bravu:302-304"]
-            "(ul. Gdańska 4a  )"                               || ["Gdańska:4a"]
-            "(ul. Gdańska 4a lok.  )"                          || ["Gdańska:4a lok."]   /// !!!!
-// TODO     "(ul. Gdańska 4a lok C4 )"                         || ["Gdańska:4a lok C4"]    - literka w nr. lokalu pierwsza
-// TODO     "(ul. Gdańska 4a lok. C4 )"                        || ["Gdańska:4a lok. C4"]   - literka pierwsza w nr. lokalu
+            "ul. Słowiańska 13/19"                                 || ["SŁOWIAŃSKA:13/19"]
+            "ul. Słowiańska 13D/19"                                || ["SŁOWIAŃSKA:13D/19"]
+            "na ul. Opolskiej 2-4"                                 || ["OPOLSKA:2-4"]
+            ///"przy ul. Łąkowej 39-44"                           || [ "Łąkowa:39-44"  -- Orzeszkowej]
+            "przy ul. Kasprzaka 39-44"                             || ["KASPRZAKA:39-44"]
+            "przy ul. Kasprzaka 302-304 ."                         || ["KASPRZAKA:302-304"]
+            "przy ul. Mihai Bravu 302-304 ."                       || ["MIHAI BRAVU:302-304"]
+            "(ul. Gdańska 4a  )"                                   || ["GDAŃSKA:4a"]
+            "(ul. Gdańska 4a lok.  )"                              || ["GDAŃSKA:4a lok."]   /// !!!!
+            "(ul. Gdańska 4a lok C4 )"                             || ["GDAŃSKA:4a lok C4"]
+            "(ul. Gdańska 4a lok. C4 )"                            || ["GDAŃSKA:4a lok. C4"]
 
-            "ul. Słowiańska 19A-19D"                           || ["Słowiańska:19A-19D"]
-            "ul. Słowiańska 19 lok. 524"                       || ["Słowiańska:19 lok. 524"]
-            "ul. Słowiańska 18 lok. 3b"                        || ["Słowiańska:18 lok. 3b"]
-            "przy ulicy Kasprzaka 39-44"                       || ["Kasprzaka:39-44"]
-            "przy alei Bohaterów Getta 39a-44c"                || ["Bohaterów Getta:39a-44c"]
+            "ul. Słowiańska 19A-19D"                               || ["SŁOWIAŃSKA:19A-19D"]
+            "ul. Słowiańska 19 lok. 524"                           || ["SŁOWIAŃSKA:19 lok. 524"]
+            "ul. Słowiańska 18 lok. 3b"                            || ["SŁOWIAŃSKA:18 lok. 3b"]
+            "przy ulicy Kasprzaka 39-44"                           || ["KASPRZAKA:39-44"]
+            "przy alei Bohaterów Getta 39a-44c"                    || ["BOHATERÓW GETTA:39a-44c"]
+
+            "Adres siedziby: Warszawa, ul. Orzeszkowej"            || ["ORZESZKOWEJ:"]
+            "miejscem jest Poznań, ul. Komisji Edukacji Narodowej" || ["KOMISJI EDUKACJI NARODOWEJ:"]
 
 
             // bo mała litera na początku następnego słowa delimituje nazwę !!!!! :-)
