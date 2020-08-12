b04dfd6363dfd5fb669dbad54cb1121a7ef0b830
Michał Olek <michal.olek@pwr.edu.pl>
2020-08-12 17:28:45 +0200

    Zachowywanie wielkich/małych liter w numerach domów

--- src/main/java/pl/clarin/pwr/g419/text/normalization/NormalizerStringToUpperCaseCutPreLastSpace.java
+++ src/main/java/pl/clarin/pwr/g419/text/normalization/NormalizerStringToUpperCaseCutPreLastSpace.java
@@ -7,10 +7,13 @@ public class NormalizerStringToUpperCaseCutPreLastSpace extends Normalizer<Strin
   public String normalize(final String value) {
     if (value == null) return null;
 
-    String result = value.toUpperCase();
-    if (result.indexOf("LOK.") == -1) {
+    String result = value; //.toUpperCase();
+    if (
+        (result.indexOf("LOK") == -1)
+            && (result.indexOf("Lok") == -1)
+            && (result.indexOf("lok") == -1)) {
       if (result.length() > 2) {
-        int index = result.length() - 1 - 1;
+        final int index = result.length() - 1 - 1;
         if (result.charAt(index) == ' ') {
           result = result.substring(0, index) + result.substring(index + 1);
         }
